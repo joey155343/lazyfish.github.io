ae28e8d1b9e06f34e87468c0055791841378ecd6
joey155343 <joey155343@hotmail.com>
2022-02-03 21:56:01 +0800

    原始狀態

--- README.md
+++
@@ -1,1 +0,0 @@
-# lazyfish.github.io
---
+++ dev/docs/.vuepress/config.js
@@ -0,0 +1,37 @@
+module.exports = {
+    title: 'Hello VuePress',
+    description: 'Just playing around',
+    themeConfig: {
+        //導航欄
+        nav: [
+            { text: 'Home', link: '/', icon: 'reco-home' },  //text:導航標題內容，icon：圖示樣式
+            { text: 'TimeLine', link: '/timeline/', icon: 'reco-date' },
+            {
+                text: 'Docs',
+                icon: 'reco-message',
+                items: [
+                    { text: 'vuepress-reco', link: '/docs/theme-reco/' }  //item： 子導航
+                ]
+            },
+            {
+                text: 'Contact',
+                icon: 'reco-message',
+                items: [
+                    { text: 'GitHub', link: 'https://github.com/recoluan', icon: 'reco-github' }
+                ]
+            }
+        ],
+        sidebar: [
+            '/StockProfolioDocs/',
+            '/StockProfolioDocs/Category_1/',
+            {
+                title: '類別2',
+                path: '/StockProfolioDocs/Category_2',
+                children: [
+                    '/StockProfolioDocs/Category_2/test1.md',
+                    '/StockProfolioDocs/Category_2/test2.md',
+                ]
+            }
+        ]
+    }
+}
